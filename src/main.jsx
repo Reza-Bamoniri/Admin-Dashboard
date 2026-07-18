8c531f71d0422eaf84bd143f0f513db63c88e447
Reza_Bamoniri <rezabri806@gmail.com>
2026-07-18 15:46:36 +0330

    feat: create style files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import "../public/styles/app.css"
+import "../public/styles/fonts.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
