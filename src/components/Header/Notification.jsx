813681c238ad6d1124d80e283234e5bcddb00049
Reza_Bamoniri <rezabri806@gmail.com>
2026-07-19 19:56:45 +0330

    feat: create notification component and add styles

--- src/components/Header/Notification.jsx
+++ src/components/Header/Notification.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { BiBell } from 'react-icons/bi'
 
 const Notification = () => {
   return (
-    <div>Notification</div>
+    <button className="flex size-10 border primary-border-color rounded-full items-center justify-center cursor-pointer hover:text-white hover:bg-blue-600">
+          <BiBell className="text-[21px]" />
+        </button>
   )
 }
 
