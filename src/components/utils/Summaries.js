bfe44436de02ce8bde2c7cbc561c80e4d5afb32f
Reza_Bamoniri <rezabri806@gmail.com>
2026-07-19 22:31:18 +0330

    feat: create summaries info file

---
+++ src/components/utils/Summaries.js
@@ -0,0 +1,39 @@
+import { BiShoppingBag } from "react-icons/bi";
+import { HiDocumentText, HiUsers } from "react-icons/hi";
+import { RiAdminFill } from "react-icons/ri";
+
+const generateSummaries = (
+  productsLength = 0,
+  usersLength = 0,
+  ticketsLength = 0,
+  adminsLength = 0,
+) => {
+  return [
+    {
+      id: 1,
+      title: "Number of Products",
+      value: productsLength,
+      Icon: BiShoppingBag,
+    },
+    {
+      id: 2,
+      title: "Number of Users",
+      value: usersLength,
+      Icon: HiUsers,
+    },
+    {
+      id: 3,
+      title: "Number of Tickets",
+      value: ticketsLength,
+      Icon: HiDocumentText,
+    },
+    {
+      id: 4,
+      title: "Number of Managers",
+      value: adminsLength,
+      Icon: RiAdminFill,
+    },
+  ];
+};
+
+export { generateSummaries };
