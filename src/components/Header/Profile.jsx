b61a14f816671b3e72bd277bdfcc8d0aa31f3cd2
Reza_Bamoniri <rezabri806@gmail.com>
2026-07-19 20:04:32 +0330

    feat: create profile  and add styles

--- src/components/Header/Profile.jsx
+++ src/components/Header/Profile.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div
+      tabindex="1"
+      className="flex focus-within:ring-2 focus-within:ring-black/20 cursor-pointer items-center gap-2 select-none p-3 hover:bg-black/5 rounded-md duration-150"
+    >
+      <div className="size-10">
+        <img
+          className="size-full rounded-full object-cover"
+          src="/images/avatar.jpg"
+          alt="User Avatar"
+        />
+      </div>
+      <div>
+        <p className="text-sm font-bold">Sarah Danes</p>
+        <p className="text-xs text-gray-600">manager</p>
+      </div>
+    </div>
   )
 }
 
