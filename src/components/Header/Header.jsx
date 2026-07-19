f2e79be02a3e09750e45d57854831fc4f250cfc9
Reza_Bamoniri <rezabri806@gmail.com>
2026-07-19 20:37:32 +0330

    style: update header  styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import Profile from './Profile'
 
 const Header = () => {
   return (
-    <div className='w-full flex justify-between *:gap-3 items-center shadow-[inset_0_-8px_10px_-10px_rgba(11,61,136,0.9)] h-35'> 
+    <div className='w-full flex justify-between relative z-10 *:gap-3 items-center shadow-[inset_0_-8px_10px_-10px_rgba(11,61,136,0.9)] h-35'> 
           <div>
                <SearchInput/>
           </div>
