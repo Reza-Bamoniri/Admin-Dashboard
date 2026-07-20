03ee09cc929774354d3f1aea3b11d95a3b5c10c9
Reza_Bamoniri <rezabri806@gmail.com>
2026-07-20 17:05:46 +0330

    feat:create Table and TableHeader components and add styles

---
+++ src/components/common/Table/Table.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import TableHeader from './elements/TableHeader'
+
+const Table = () => {
+
+
+    let header = { title:undefined, Buttons: undefined}
+
+  return (
+    <div className="mt-10 border min-w-full! bg-white primary-border-color rounded-xl overflow-hidden">
+        <TableHeader header={header} />
+      <div>{children}</div>
+    </div>
+  )
+}
+
+export default Table
