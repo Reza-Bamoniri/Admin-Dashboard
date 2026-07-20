bb029dfad774aa4c561b9d79644f8db79e3b47a6
Reza_Bamoniri <rezabri806@gmail.com>
2026-07-21 01:35:38 +0330

    feat: Add the ability to display or not display modal

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BiX } from 'react-icons/bi'
 
-const Modal = ({Trigger}) => {
+const Modal = ({Trigger, title, children}) => {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const closeModal = () => setIsOpen(false)
+  const openModal = () => setIsOpen(true)
+
+
   return (
-    <div>{Trigger}</div>
+
+    <>
+    <div onClick={openModal} >{Trigger}</div>
+    
+      <div
+        id="modal-screen"
+        className={
+          
+          isOpen ? "w-dvw h-dvh right-0 top-0 fixed z-10 bg-black/30 flex-center duration-300" : " w-dvw h-dvh right-0 top-0 fixed z-10 bg-black/30 flex-center duration-300 invisible opacity-0"
+        }
+      >
+        <div className="w-125 rounded-lg overflow-hidden bg-white drop-shadow-2xl/30 max-h-[90dvh] overflow-y-auto">
+          <div className="flex items-center justify-between px-4 h-16">
+            <span className="text-[17px] text-zinc-800 font-bold">{title}</span>
+            <button
+              onClick={closeModal}
+              className="text-2xl! text-zinc-500 cursor-pointer"
+            >
+              <BiX />
+            </button>
+          </div>
+          <div
+            id="modal-content"
+            className="p-4 py-5 border-y primary-border-color"
+          >
+            {children}
+          </div>
+
+          <div className="min-h-14 flex items-center justify-end gap-2 px-4 bg-[#F6F8FA]">
+            {/* <ModalFooter onSubmit={onSubmit} onClose={closeModal} /> */}
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
